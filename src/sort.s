751b846e887cba9d62f1f15b6a291192711a2d57
esvault <54893829+esvault@users.noreply.github.com>
2020-10-19 12:38:41 +0300

    Update sort.s

--- src/sort.s
+++ src/sort.s
@@ -1,12 +1,11 @@
 	.file	"sort.c"
 	.text
-	.p2align 4
+	.p2align 4,,15
 	.globl	swap
 	.type	swap, @function
 swap:
 .LFB0:
 	.cfi_startproc
-	endbr64
 	movl	(%rdi), %eax
 	movl	(%rsi), %edx
 	movl	%edx, (%rdi)
@@ -15,58 +14,51 @@ swap:
 	.cfi_endproc
 .LFE0:
 	.size	swap, .-swap
-	.p2align 4
+	.p2align 4,,15
 	.globl	sort_array
 	.type	sort_array, @function
 sort_array:
 .LFB1:
 	.cfi_startproc
-	endbr64
-	cmpq	$1, %rsi
-	jbe	.L3
-	movq	%rdi, %r8
+	testl	%esi, %esi
+	jle	.L3
+	leal	-1(%rsi), %r11d
 	movl	$1, %r9d
+	addq	$4, %rdi
+	movq	%rdi, %r8
+	leal	-2(%rsi), %r10d
+	addq	$1, %r11
+	cmpq	%r11, %r9
+	je	.L3
 	.p2align 4,,10
 	.p2align 3
-.L5:
-	movq	%r9, %rax
+.L9:
+	movl	%r10d, %eax
+	addq	%r9, %rax
+	leaq	(%rdi,%rax,4), %rsi
+	movq	%r8, %rax
 	.p2align 4,,10
 	.p2align 3
-.L7:
-	movl	(%r8), %edx
-	movl	(%rdi,%rax,4), %ecx
-	cmpl	%ecx, %edx
-	jle	.L6
-	movl	%ecx, (%r8)
-	movl	%edx, (%rdi,%rax,4)
 .L6:
-	addq	$1, %rax
-	cmpq	%rax, %rsi
-	ja	.L7
+	movl	-4(%r8), %edx
+	movl	(%rax), %ecx
+	cmpl	%ecx, %edx
+	jle	.L5
+	movl	%ecx, -4(%r8)
+	movl	%edx, (%rax)
+.L5:
+	addq	$4, %rax
+	cmpq	%rsi, %rax
+	jne	.L6
 	addq	$1, %r9
 	addq	$4, %r8
-	cmpq	%r9, %rsi
-	jne	.L5
+	subl	$1, %r10d
+	cmpq	%r11, %r9
+	jne	.L9
 .L3:
-	ret
+	rep ret
 	.cfi_endproc
 .LFE1:
 	.size	sort_array, .-sort_array
-	.ident	"GCC: (Ubuntu 9.3.0-10ubuntu2) 9.3.0"
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
 	.section	.note.GNU-stack,"",@progbits
-	.section	.note.gnu.property,"a"
-	.align 8
-	.long	 1f - 0f
-	.long	 4f - 1f
-	.long	 5
-0:
-	.string	 "GNU"
-1:
-	.align 8
-	.long	 0xc0000002
-	.long	 3f - 2f
-2:
-	.long	 0x3
-3:
-	.align 8
-4:
