021695f5278d05dc4ab601c1b81a280e69072dc3
esvault <54893829+esvault@users.noreply.github.com>
2020-10-19 14:18:41 +0300

    searching...

--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+int do_something(int cool) {
+    return ++cool;
+}
+
 int main(int argc, char *argv[]) {
+    if (argv) {
+        int cool = 0;
+        do_something(cool);
+    }
     if (argc == 1) {
         return -1;
     } else if (argc == 2) {
