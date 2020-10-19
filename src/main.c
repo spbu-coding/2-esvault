ca09896a27dbd01ea8e83d4b4af999ab722dc022
esvault <54893829+esvault@users.noreply.github.com>
2020-10-19 14:21:47 +0300

    mini searching...

--- src/main.c
+++ src/main.c
@@ -12,7 +12,8 @@ int main(int argc, char *argv[]) {
     if (argc == 1) {
         return -1;
     } else if (argc == 2) {
-        fprintf(stdout, "%d %d", 2, 1);
+        fprintf(stdout, "%d ", 2);
+        fprintf(stdout, "%d ", 1);
         return 3;
     } else if (argc == 3) {
         fprintf(stdout, "%d ", 1);
